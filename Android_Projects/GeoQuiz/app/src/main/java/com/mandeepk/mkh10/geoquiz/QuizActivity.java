d895448166ed4860ceda8880dfedbd168caf9d91
Khataria, Mandeep <mfiddy23@gmail.com>
2014-09-16 15:52:10 +0100

    Finished the code base for chapter 1

--- Android_Projects/GeoQuiz/app/src/main/java/com/mandeepk/mkh10/geoquiz/QuizActivity.java
+++ Android_Projects/GeoQuiz/app/src/main/java/com/mandeepk/mkh10/geoquiz/QuizActivity.java
@@ -1,11 +1,12 @@
 package com.mandeepk.mkh10.geoquiz;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class QuizActivity extends ActionBarActivity {
@@ -22,10 +23,17 @@ public class QuizActivity extends ActionBarActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener(){
             @Override
                 public void onClick(View v) {
-                //Does nothing yet but soon!
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
