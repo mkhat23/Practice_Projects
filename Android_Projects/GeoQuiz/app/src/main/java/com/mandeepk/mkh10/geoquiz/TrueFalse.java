48589c77ef5ea6cf5c58653e2b3fe5ba859451fa
Khataria, Mandeep <mfiddy23@gmail.com>
2014-09-18 16:28:17 +0100

    New TrueFalse class following MVC

---
+++ Android_Projects/GeoQuiz/app/src/main/java/com/mandeepk/mkh10/geoquiz/TrueFalse.java
@@ -0,0 +1,31 @@
+package com.mandeepk.mkh10.geoquiz;
+
+/**
+ * Created by mkh10 on 18/09/2014.
+ */
+public class TrueFalse {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion){
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+}
