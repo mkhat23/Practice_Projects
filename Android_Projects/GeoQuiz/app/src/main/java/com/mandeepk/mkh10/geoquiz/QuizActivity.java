242e2ffc2659282d4809709fc0a69e475a0583ab
Khataria, Mandeep <mfiddy23@gmail.com>
2014-10-20 15:59:23 +0100

    updated changes for chapter 2 to adhere to MVC

--- Android_Projects/GeoQuiz/app/src/main/java/com/mandeepk/mkh10/geoquiz/QuizActivity.java
+++ Android_Projects/GeoQuiz/app/src/main/java/com/mandeepk/mkh10/geoquiz/QuizActivity.java
@@ -1,57 +1,69 @@
 package com.mandeepk.mkh10.geoquiz;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+public class QuizActivity extends Activity {
+	
+    Button mTrueButton;
+    Button mFalseButton;
+    Button mNextButton;
 
-public class QuizActivity extends ActionBarActivity {
-
-    private Button mTrueButton;
-    private Button mFalseButton;
-    private ImageButton mNextButton;
-    private ImageButton mPreviousButton;
-    private TextView mQuestionTextView;
-
-    private TrueFalse[] mQuestionBank = new TrueFalse[]{
+    TextView mQuestionTextView;
+    
+    TrueFalse[] mAnswerKey = new TrueFalse[] {
             new TrueFalse(R.string.question_oceans, true),
             new TrueFalse(R.string.question_mideast, false),
             new TrueFalse(R.string.question_africa, false),
             new TrueFalse(R.string.question_americas, true),
-            new TrueFalse(R.string.question_asia, true),
+            new TrueFalse(R.string.question_asia, true)
     };
+    
+    int mCurrentIndex = 0;
+    
+    private void updateQuestion() {
+        int question = mAnswerKey[mCurrentIndex].getQuestion();
+        mQuestionTextView.setText(question);
+    }
+    
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mAnswerKey[mCurrentIndex].isTrueQuestion();
+        
+        int messageResId = 0;
+        
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
 
-    private int mCurrentIndex = 0;
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+            .show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-        mQuestionTextView.setOnClickListener(new TextView.OnClickListener(){
-            @Override
-        public void onClick(View v) {
-            mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                updateQuestion();
-            }
-        });
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        //set text to first question in array
 
-        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton = (Button)findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 checkAnswer(true);
             }
         });
 
-        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton = (Button)findViewById(R.id.false_button);		
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,64 +71,27 @@ public class QuizActivity extends ActionBarActivity {
             }
         });
 
-        mNextButton = (ImageButton) findViewById(R.id.next_button);
+        mNextButton = (Button)findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                mCurrentIndex = (mCurrentIndex + 1) % mAnswerKey.length; 
                 updateQuestion();
             }
         });
-
-        mPreviousButton = (ImageButton) findViewById(R.id.previous_button);
-        mPreviousButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-        public void onClick (View v){
-                mCurrentIndex =  (mCurrentIndex - 1) % mQuestionBank.length;
-                updateQuestion();
-            }
-        });
-    }
-
-    private void updateQuestion() {
-        int question = mQuestionBank[mCurrentIndex].getQuestion();
-        mQuestionTextView.setText(question);
-    }
-
-    private void checkAnswer(boolean userPressedTrue) {
-        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
-
-        int messageResId = 0;
-
-        if (userPressedTrue == answerIsTrue) {
-            messageResId = R.string.correct_toast;
-        } else {
-            messageResId = R.string.incorrect_toast;
-        }
-
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
-
+        
+        updateQuestion();
     }
 
 
-        @Override
-        public boolean onCreateOptionsMenu (Menu menu){
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.quiz, menu);
-            return true;
-        }
 
-        @Override
-        public boolean onOptionsItemSelected (MenuItem item){
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            int id = item.getItemId();
-            if (id == R.id.action_settings) {
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_quiz, menu);
+        return true;
     }
 
-
+}
--- Android_Projects/GeoQuiz/app/src/main/java/com/mandeepk/mkh10/geoquiz/TrueFalse.java
+++ Android_Projects/GeoQuiz/app/src/main/java/com/mandeepk/mkh10/geoquiz/TrueFalse.java
@@ -1,31 +1,30 @@
 package com.mandeepk.mkh10.geoquiz;
 
-/**
- * Created by mkh10 on 18/09/2014.
- */
 public class TrueFalse {
     private int mQuestion;
+    // this variable will hold a resource ID for a string
 
     private boolean mTrueQuestion;
-
-    public TrueFalse(int question, boolean trueQuestion){
+    
+    public TrueFalse(int question, boolean trueQuestion) {
         mQuestion = question;
         mTrueQuestion = trueQuestion;
     }
 
-    public int getQuestion() {
-        return mQuestion;
-    }
+	public int getQuestion() {
+		return mQuestion;
+	}
 
-    public void setQuestion(int question) {
-        mQuestion = question;
-    }
+	public void setQuestion(int question) {
+		mQuestion = question;
+	}
 
-    public boolean isTrueQuestion() {
-        return mTrueQuestion;
-    }
+	public boolean isTrueQuestion() {
+		return mTrueQuestion;
+	}
 
-    public void setTrueQuestion(boolean trueQuestion) {
-        mTrueQuestion = trueQuestion;
-    }
+	public void setTrueQuestion(boolean trueQuestion) {
+		mTrueQuestion = trueQuestion;
+	}
+
 }
